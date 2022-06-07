9463cf39ad29b014d6e2d1ecf52d59cafa8ef748
aTfure <atfure@gmail.com>
2022-06-07 11:31:16 +0000

    Nested while

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,6 +12,7 @@
 int numLength(int num)
 {
 	int length = 0;
+
 	if (!num)
 	return (1);
 	while (num)
@@ -22,12 +23,11 @@ int numLength(int num)
 	return (length);
 }
 
+
+
 /**
  * main - Entry point
  * Description: prints the first 98 Fibonacci numbers
-   starting with 1 and 2 followed by a new line
- 
- * Solution was copied from Nobert Patrick Wise, github handle: Trikcode
  
  * Return: Always 0 (Success)
  */
@@ -36,6 +36,7 @@ int main(void)
 {
 	int count, initial0s;
 	unsigned long f1 = 1, f2 = 2, sum, mx = 100000000, f1o = 0, f2o = 0, sumo = 0;
+
 	for (count = 1; count <= 98; ++count)
 	{
 	if (f1o > 0)
