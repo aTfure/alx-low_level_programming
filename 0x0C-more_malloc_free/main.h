cb8a2cb7fa7226bf94aa42b3663008922b73d6ef
aTfure <atfure@gmail.com>
2022-07-01 07:24:10 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -23,4 +23,10 @@ void init(char *str, int l);
 
 int main(int argc, char *argv[]);
 
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 #endif
