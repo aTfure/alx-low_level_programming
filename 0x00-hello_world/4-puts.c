8730fe4c5e82bb01cc9a3c02833eb37bcb36a629
aTfure <atfure@gmail.com>
2024-08-15 16:18:33 +0300

    using function puts for return

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * Using Function puts
+ */
+
+int main(void)
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(Mystr);
+
+	return 0;
+}
