139aa5f6958f34e6e54ce74948deb8658ce9976a
aTfure <atfure@gmail.com>
2024-08-16 03:34:28 +0300

    print that does not allow use of printf and puts then return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	fwrite(str, sizeof(str), 1, stderr);
+	return (1);
+}
