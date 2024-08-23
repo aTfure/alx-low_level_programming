1ed6f11037c27002650c4bb63712b2ab840f4fdc
aTfure <atfure@gmail.com>
2024-08-23 05:19:29 +0300

    unction that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: check the character
+ *
+ * Return: 0 Always(Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+
+	else
+	{
+		return (n);
+	}
+}
