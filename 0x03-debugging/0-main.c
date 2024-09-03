c89752ce10d2990e04695177528f1f277dac8899
aTfure <atfure@gmail.com>
2024-09-03 06:28:40 +0300

    debugging; checking is a number is positive, negative or zero

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * void positive_or_negative - test the positive_or_negative function with 0
+ *
+ * Return: 0 Always(Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
